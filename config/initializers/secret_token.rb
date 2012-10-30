1d895fc268fb1d54a057ee64eb908beff4c2b893
Vivek <vivek@ubuntu.(none)>
2012-10-30 21:04:52 +0530

    Initializing application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloApp::Application.config.secret_token = 'd238ac446e029592a8030b34806d001af48323f1b1b720a2eaed83b97ed0cea6848f3b30b7545137ccc69dc34edd674ad11466fbd55eb1d40df10cfdc3396eab'
